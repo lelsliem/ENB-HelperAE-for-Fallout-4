c06d923be3e8a7498fc43f39316e60d21d854769
lelsliem <mrlmarkwick@gmail.com>
2026-08-04 20:46:07 +0100

    Refactor ENBHelper interface and add new functions

--- src/enbhelper.h
+++ src/enbhelper.h
@@ -1,17 +1,78 @@
 #pragma once
 
+#include <shared_mutex>
+#include "RE/Fallout.h"
+
+#ifdef _WIN32
+#define DLLEXPORT __declspec(dllexport)
+#else
+#define DLLEXPORT
+#endif
+
+// Plugin global flags
 extern bool bLoaded;
 
+// Shared state mutex and cached data struct
+extern std::shared_mutex stateMutex;
+
+struct ThreadCachedData {
+    float time = 0.0f;
+    float weatherTransition = 0.0f;
+    unsigned long currentWeatherID = 0;
+    unsigned long outgoingWeatherID = 0;
+    int currentWeatherClass = -1;
+    int outgoingWeatherClass = -1;
+    unsigned long locationID = 0;
+    unsigned long worldSpaceID = 0;
+    unsigned long skyMode = 0;
+    bool isInterior = false;
+    RE::NiTransform cameraLocal;
+    RE::NiTransform cameraWorld;
+    RE::NiTransform cameraOldWorld;
+};
+
+extern ThreadCachedData cachedData;
+
+// Compact health struct returned by GetHealthStatus
+struct ENBHelperHealth {
+    float in_game_time;
+    uint32_t current_weather_id;
+    uint32_t outgoing_weather_id;
+    int32_t current_weather_class;
+    int32_t outgoing_weather_class;
+    uint32_t worldspace_id;
+    uint32_t location_id;
+    uint32_t sky_mode;
+    int32_t is_interior; // 0 or 1
+};
+
 extern "C" {
-	__declspec(dllexport) bool GetTime(float& time);
-	__declspec(dllexport) bool GetWeatherTransition(float& t);
-	__declspec(dllexport) bool GetCurrentWeather(unsigned long& id);
-	__declspec(dllexport) bool GetOutgoingWeather(unsigned long& id);
-	__declspec(dllexport) bool GetCurrentWeatherClassification(int& c);
-	__declspec(dllexport) bool GetOutgoingWeatherClassification(int& c);
-	__declspec(dllexport) bool GetCurrentLocationID(unsigned long& id);
-	__declspec(dllexport) bool GetWorldSpaceID(unsigned long& id);
-	__declspec(dllexport) bool GetSkyMode(unsigned long& mode);
-	__declspec(dllexport) bool GetPlayerCameraTransformMatrices(RE::NiTransform& m_local, RE::NiTransform& m_world, RE::NiTransform& m_oldworld);
-	__declspec(dllexport) bool IsLoaded();
+
+// Basic getters
+DLLEXPORT bool GetTime(float& time);
+DLLEXPORT bool GetWeatherTransition(float& t);
+DLLEXPORT bool GetCurrentWeather(unsigned long& id);
+DLLEXPORT bool GetOutgoingWeather(unsigned long& id);
+DLLEXPORT bool GetCurrentWeatherClassification(int& c);
+DLLEXPORT bool GetOutgoingWeatherClassification(int& c);
+DLLEXPORT bool GetCurrentLocationID(unsigned long& id);
+DLLEXPORT bool GetWorldSpaceID(unsigned long& id);
+DLLEXPORT bool GetSkyMode(unsigned long& mode);
+DLLEXPORT bool GetPlayerCameraTransformMatrices(RE::NiTransform& m_local, RE::NiTransform& m_world, RE::NiTransform& m_oldworld);
+DLLEXPORT bool GetIsInterior(bool& isInterior);
+
+// ReShade bridge pointer (returns pointer to internal bridge struct)
+DLLEXPORT void* GetReShadeBridgePointer();
+
+// Health and version
+DLLEXPORT bool GetHealthStatus(ENBHelperHealth* out);
+DLLEXPORT float GetPluginVersion();
+
+// F4SE plugin interface
+DLLEXPORT bool F4SEAPI F4SEPlugin_Query(const F4SE::QueryInterface* a_f4se, F4SE::PluginInfo* a_info);
+DLLEXPORT bool F4SEAPI F4SEPlugin_Load(const F4SE::LoadInterface* a_f4se);
+
+// Utility
+DLLEXPORT bool IsLoaded();
+
 }
